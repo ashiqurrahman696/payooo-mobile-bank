39467746eb2cf32a435bd1453267bc5ac1a96fd3
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-08-24 17:29:35 +0600

    updated mobile number validation

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ loginBtn.addEventListener('click', login);
 
 function login(e){
     e.preventDefault();
-    let mobileNumber = "01963836103";
     let pinNumber = 1234;
     let mobileNumberInput = document.getElementById('mobile-number');
     let mobileNumberValue = mobileNumberInput.value;
     let pinNumberInput = document.getElementById('pin-number');
     let pinNumberValue = parseInt(pinNumberInput.value);
-    if(mobileNumberValue === mobileNumber && pinNumberValue === pinNumber){
+    if(mobileNumberValue.length === 11 && pinNumberValue === pinNumber){
         window.location.href = "home.html";
     }
     else{
